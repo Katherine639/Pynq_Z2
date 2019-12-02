b20fac7ae5d449854d21eb6adfe5c1b0b36ffa8d
Katherine639 <kway001@ucr.edu>
2019-12-02 05:16:12 -0800

    Update object_tracking_with_color.py

--- object_tracking_with_color.py
+++ object_tracking_with_color.py
@@ -1,8 +1,3 @@
-
-# USAGE
-# python object_movement.py --video object_tracking_example.mp4
-# python object_movement.py
-
 import imutils
 # import the necessary packages
 from collections import deque
